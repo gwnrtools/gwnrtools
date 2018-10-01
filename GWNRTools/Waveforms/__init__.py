9068c0fe1c81c7ba92f50aed8d437f5d11e5b0b4
Prayush Kumar <prayush.kumar@ligo.org>
2018-10-01 01:41:04 -0400

    add script to join HDF data files

--- GWNRTools/Waveforms/__init__.py
+++ GWNRTools/Waveforms/__init__.py
@@ -1,2 +1,4 @@
 from GWNRTools.Waveforms.EccentricUtilities import *
+from GWNRTools.Waveforms.nr_waveform_sxs.py import *
+from GWNRTools.Waveforms.TidalWaveforms import *
 from GWNRTools.Waveforms.WaveformUtilities import *
