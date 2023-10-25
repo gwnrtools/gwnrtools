4d7750b775de73d81db3a37a0187ac1c76ac8473
Prayush Kumar <prayush@users.noreply.github.com>
2023-10-25 15:52:31 +0530

    Delete __init__.py as this was a hack which is no longer required

--- __init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-# Copyright (c) 2018, Prayush Kumar
-# See LICENSE file for details:
-# <https://github.com/gwnrtools/gwnrtools/blob/master/LICENSE>
-
-from __future__ import print_function, division, absolute_import
-from .gwnr import *
